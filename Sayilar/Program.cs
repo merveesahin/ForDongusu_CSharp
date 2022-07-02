4754adc6619d120914cb86b4515a1b400357a46b
oguze <oguze@devbox>
2022-07-03 01:19:45 +0300

    Add project files.

---
+++ Sayilar/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Sayilar
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Sayilar = { 1, 2, 3, 4, 5 };
+            for (int i = 0; i < Sayilar.Length; i++)
+            {
+                for(int j = 0; j < Sayilar.Length; j++)
+                {
+                    Console.WriteLine(Sayilar[i]+ " " +Sayilar[j]);
+                }
+   
+            }
+
+        }
+    }
+}
